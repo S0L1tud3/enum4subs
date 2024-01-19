d0326a7bee1019a3a6a2390fb644aab594314cfb
S0L1tud3 <71250863+S0L1tud3@users.noreply.github.com>
2024-01-20 00:34:27 +0800

    Update enum4subs.sh

--- enum4subs.sh
+++ enum4subs.sh
@@ -45,8 +45,9 @@ function probing_subs {
 
   echo -e "${b_color_purple}-- Probing All Sorted Subdomains..${normal}\n"
   ### Test for Now #########
-  cat "${sorted}/enum4subs_allsubs.txt" | httprobe -c 80 | cut -d '/' -f 3 | sort -u >> "${sorted}/httprobe/enum4subs_allsubs_httprobe.txt"
-  #nmap -sV -vv -iL "${sorted}/httprobe/enum4subs_allsubs_httprobe.txt" -oA "${sorted}/httprobe/nmap"
+  cat "${sorted}/enum4subs_allsubs.txt" | httprobe -c 80 >> "${sorted}/httprobe/enum4subs_allsubs_httprobe.txt"
+  cat "${sorted}/httprobe/enum4subs_allsubs_httprobe.txt" | cut -d '/' -f 3 | sort -u >> "${sorted}/httprobe/enum4subs_allsubs_httprobe-sorted.txt"
+  #nmap -sV -vv -iL "${sorted}/httprobe/enum4subs_allsubs_httprobe-sorted.txt" -oA "${sorted}/httprobe/nmap"
   ##########################
   httpx -l "${sorted}/enum4subs_allsubs.txt" -silent -td -cname -vhost -sc -title -cl -ct -t 80 -ip -o "${sorted}/enum4subs_allsubs_httpx.txt"
   echo -e "\n${b_color_purple}-- Sorting Subdomains by Status Codes ${normal}\n"
